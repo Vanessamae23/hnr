f844973fcef70815c18271af44eedb11cf21f885
HEEaZ <do.u.noe.imm@gmail.com>
2024-01-21 05:28:50 +0800

    Style header

--- project/src/components/Header.tsx
+++ project/src/components/Header.tsx
@@ -8,7 +8,7 @@ import logoModMatch from '../assets/modmatch-logo.png';
 
 export default function Header() {
   return (
-    <AppBar sx={{ flexDirection: 'row', alignItems: 'center', background: "#fff", width: '100%', }}>
+    <AppBar sx={{ flexDirection: 'row', alignItems: 'center', background: "#fff", width: '100%', backgroundColor: "#F3F3F3"}} elevation={0}>
       <Toolbar sx={{ zIndex: 2, justifyContent: 'center', alignItems: 'center'}}>
         <Box sx={{display: 'flex', alignItems: 'auto'}}>
           <img src={logoModMatch} alt="ModMatch logo" style={{ height: 50, width: 'auto' }}/>
