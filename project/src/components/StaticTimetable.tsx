fa828bc018e2ccfa9e7fe820e2f85dd23bcc756a
MadeEasy <vanessamae23@gmail.com>
2024-01-21 01:41:07 +0800

    Add static timetable

---
+++ project/src/components/StaticTimetable.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Epg, Layout, Program, ProgramItem } from "planby";
+
+// Import hooks
+import { useStaticApp } from "../useApp";
+import { Timeline } from "./Timeline";
+import { ModuleItem } from "./ModuleItem";
+import { DayItem } from "./DayItem";
+import {
+  classesToPrograms,
+} from "../utils/data";
+
+const StaticTimetable = ({ classes }) => {
+  
+  const { isLoading, getEpgProps, getLayoutProps } = useStaticApp(classesToPrograms(classes));
+
+  return (
+    <div style={{ height: "60vh", width: "100%" }}>
+      <Epg isLoading={isLoading} {...getEpgProps()}>
+        <Layout
+          {...getLayoutProps()}
+          renderTimeline={(props) => <Timeline {...props} />}
+          renderProgram={({ program, ...rest }) => {
+            return (
+              
+                <ModuleItem key={program.data.id} program={program} {...rest} />
+            );
+          }}
+          renderChannel={({ channel }) => (
+            <DayItem key={channel.uuid} channel={channel} />
+          )}
+        />
+      </Epg>
+    </div>
+  );
+};
+
+export default StaticTimetable;
--- project/src/useApp.tsx
+++ project/src/useApp.tsx
@@ -140,3 +140,46 @@ export function useFriendApp(epgList: Program[], personName) {
     isLoading,
   };
 }
+
+export function useStaticApp(epgList: Program[]) {
+    const [channels, setChannels] = React.useState<Channel[]>([]);
+    const [epg, setEpg] = React.useState<Program[]>(epgList);
+    const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  
+    const channelsData = React.useMemo(() => channels, [channels]);
+    const epgData = React.useMemo(() => epg, [epg]);
+  
+    const { getEpgProps, getLayoutProps } = useEpg({
+      channels: channelsData,
+      epg: epgData,
+      dayWidth: 1500,
+      sidebarWidth: 60,
+      itemHeight: 70,
+      isSidebar: true,
+      isTimeline: true,
+      isLine: true,
+      startDate: "2024-01-20T07:00:00",
+      endDate: "2024-01-20T23:00:00",
+      isBaseTimeFormat: true,
+      theme,
+    });
+  
+ 
+    const handleFetchResources = React.useCallback(async () => {
+      setIsLoading(true);
+      const channels = await fetchChannels();
+      setChannels(channels as Channel[]);
+      setIsLoading(false);
+    }, []);
+  
+    React.useEffect(() => {
+      handleFetchResources();
+    }, [handleFetchResources]);
+  
+    return {
+      getEpgProps,
+      getLayoutProps,
+      isLoading,
+    };
+  }
+  
