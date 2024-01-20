0d3b7efb23ad1ee9f0d83941358239ecdc812fe9
HEEaZ <do.u.noe.imm@gmail.com>
2024-01-20 18:46:11 +0800

    Create defaults

---
+++ project/src/defaults/default.ts
@@ -0,0 +1,8 @@
+import { LocalStorage_Me } from "../types/types"
+
+export const default_LocalStorage_Me : LocalStorage_Me = {
+    name: "↑", // Some untypable character that hopefully nobody uses
+    link: "",
+    blockout: [],
+    classes: [],
+  }
