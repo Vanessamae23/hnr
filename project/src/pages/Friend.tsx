057afef0039f5004ffb27fd8980a03b9f914fbdc
HEEaZ <do.u.noe.imm@gmail.com>
2024-01-21 06:20:28 +0800

    Add Import to Friend Timetable

--- project/src/pages/Friend.tsx
+++ project/src/pages/Friend.tsx
@@ -6,8 +6,6 @@ import { Blockout, Class, Person } from "../types/types";
 import { useParams, redirect } from "react-router-dom";
 import {
   Box,
-  TextField,
-  Typography,
   List,
   ListItem,
   ListItemText,
@@ -15,12 +13,14 @@ import {
 } from "@mui/material";
 import BlockOutForm from "../components/BlockOutForm";
 import { formatISODateToAMPM } from "../utils/utils";
-import LayoutWrapper from "./AppLayout";
+import { default_LocalStorage_Friends } from "../defaults/default";
+import { linkToClasses } from "../utils/utils";
+import ImportTimetableForm from "../components/ImportTimetableForm";
 
 function Friend() {
   const [friends, setFriends] = useLocalStorage<Person[]>(
     LOCALSTORAGE_KEY_FRIENDS,
-    []
+    default_LocalStorage_Friends
   );
   const idParam = useParams().id;
 
@@ -49,6 +49,16 @@ function Friend() {
     setFriends(updatedFriends);
   };
 
+  const [link, setLink] = React.useState(friend.link)
+  const find = (link: string) => {
+    setLink(link);
+    friend.link = link;
+    friend.classes = linkToClasses(link);
+    const updatedFriends = friends;
+    updatedFriends[id] = friend;
+    setFriends(updatedFriends)
+  };
+
   const handleDeleteBlockOut = (index: number) => {
     const updatedFriends = [...friends];
     updatedFriends[id].blockout.splice(index, 1);
@@ -59,32 +69,31 @@ function Friend() {
 
   return (
     <Box sx={{ width: "100%", maxWidth: 1200, mx: "auto", my: 4 }}>
-      <TimeTable
-        name={friend.name}
-        classes={friend.classes}
-        setClasses={setClasses}
-      />
-      <Box sx={{ margin: "16px p", padding: "64px" }}>
-        <BlockOutForm onBlockOut={handleAddBlockOut} />
-        <List>
-          {parsedBlockout.map((blockout: Blockout, index: number) => (
-            <ListItem key={index}>
-              <ListItemText
-                primary={blockout.day}
-                secondary={
-                  blockout.startTime && blockout.endTime
-                    ? formatISODateToAMPM(blockout.startTime) +
-                      " - " +
-                      formatISODateToAMPM(blockout.endTime)
-                    : ""
-                }
-              />
-              <Button onClick={() => handleDeleteBlockOut(index)}>
-                Delete
-              </Button>
-            </ListItem>
-          ))}
-        </List>
+      <Box sx={{ display: "flex", flexDirection: "column" }}>
+        <ImportTimetableForm onImport={find} initialValue={link}/>
+        <TimeTable name={friend.name} classes={friend.classes} setClasses={setClasses} />
+        <Box sx={{ margin: "16px p", padding: "64px" }}>
+          <BlockOutForm onBlockOut={handleAddBlockOut} />
+          <List>
+            {parsedBlockout.map((blockout: Blockout, index: number) => (
+              <ListItem key={index}>
+                <ListItemText
+                  primary={blockout.day}
+                  secondary={
+                    blockout.startTime && blockout.endTime
+                      ? formatISODateToAMPM(blockout.startTime) +
+                        " - " +
+                        formatISODateToAMPM(blockout.endTime)
+                      : ""
+                  }
+                />
+                <Button onClick={() => handleDeleteBlockOut(index)}>
+                  Delete
+                </Button>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
       </Box>
     </Box>
   );
