2aa2f8186d2b532ea99219ce7e227582d73e48e9
NathanielHackathonsNUS <nathaniel.hackathons.nus@gmail.com>
2024-01-20 15:21:14 +0800

    Add initial candidate finding logic

--- project/src/logic/logic.ts
+++ project/src/logic/logic.ts
@@ -1,6 +1,7 @@
+import Module from "module";
 import { N_DAYS, N_TIME_BLOCKS } from "../constants/constants";
-import { Availabilities, Availability, Grouping, ModuleDatas, Modules, TimetableInputs, Timetables, UnlockedModules, UnlockedTimetables } from "../types/types";
-import { getClasses, getLessonTypes } from "../utils/data";
+import { Availabilities, Availability, CandidateLessons, CandidateModules, CandidateTimetables, Grouping, ModuleDatas, Modules, TimetableInputs, Timetables, UnlockedModules, UnlockedTimetables } from "../types/types";
+import { getAllClassNos, getAllClasses, getClasses, getLessonTypes } from "../utils/data";
 import { parseLink } from "../utils/links";
 
 export const findValidTimetables = (timetableInputs: TimetableInputs, groupings: Grouping[]) => {
@@ -11,8 +12,10 @@ export const findValidTimetables = (timetableInputs: TimetableInputs, groupings:
   const availabilities = findAvailabilities(lockedTimetables);
 
   // Step 3: Find initial set of candidates classes based on individual availability
+  const candidateTimetables = findCandidateTimetables(unlockedTimetables, availabilities);
 
   // Step 4: Filter candidate based on group-related availabilities
+
 
   // Step 5: Depth-first search on all possible candidate class combinations
 }
@@ -99,6 +102,56 @@ const findAvailabilities = (lockedTimetables: Timetables) => {
   return availabilities;
 }
 
+const findCandidateTimetables = (unlockedTimetables: UnlockedTimetables, availabilities: Availabilities): CandidateTimetables => {
+  const candidateTimetables: CandidateTimetables = {};
+
+  for (const [person, unlockedLessons] of Object.entries(unlockedTimetables)) {
+    const availability = availabilities[person];
+
+    const candidateModules: CandidateModules = {}
+    candidateTimetables[person] = candidateModules;
+
+    for (const [moduleCode, lessonTypes] of Object.entries(unlockedLessons)) {
+      const candidateLessons: CandidateLessons = {};
+      candidateModules[moduleCode] = candidateLessons;
+
+      for (const lessonType of lessonTypes) {
+        const candidateClasses: string[] = [];
+        candidateLessons[lessonType] = candidateClasses;
+
+        const classDatas = getAllClasses(moduleCode, lessonType);
+        const classNos = getAllClassNos(moduleCode, lessonType);
+
+        for (const classNo of classNos) {
+          for (const classData of classDatas) {
+            if (classData.classNo != classNo) {
+              continue;
+            }
+
+            const { startTime, endTime, day } = classData;
+            const startIndex = timeToIndex(startTime);
+            const endIndex = timeToIndex(endTime);
+            const dayIndex = dayToIndex(day);
+            let isCandidateClass = true;
+
+            for (let i = startIndex; i < endIndex; i++) {
+              if (!availability[dayIndex][i]) {
+                isCandidateClass = false;
+                break;
+              }
+            }
+
+            if (isCandidateClass) {
+              candidateClasses.push(classNo);
+            }
+          }
+        }
+      }
+    }
+  }
+
+  return candidateTimetables;
+}
 
 
 // Utils
